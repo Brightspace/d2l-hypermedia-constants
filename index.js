6c651806e7187c3c6c0c6dc63f5f81f223c75593
Adam Palaniuk <Adam.Palaniuk@D2L.com>
2019-01-26 12:18:23 -0600

    Add my-activities#empty rel

--- index.js
+++ index.js
@@ -42,6 +42,7 @@ export const Rels = {
 	// Activities API sub-domain rels
 	Activities: {
 		myActivities: 'https://activities.api.brightspace.com/rels/my-activities',
+		myActivitiesEmpty: 'https://activities.api.brightspace.com/rels/my-activities#empty',
 		activityUsage: 'https://activities.api.brightspace.com/rels/activity-usage',
 		myActivityUsage: 'https://activities.api.brightspace.com/rels/my-activity-usage',
 		userActivityUsage: 'https://activities.api.brightspace.com/rels/user-activity-usage',
